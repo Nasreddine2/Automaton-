206d7e3360dc1d254985208ecccdd768b7bc8626
Nasreddine2 <hammoudinasreddine2002@gmail.com>
2024-09-29 01:32:17 +0200

    Commit

--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,35 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware pour gérer CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permet toutes les origines
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // Endpoint pour télécharger un livre depuis Gutenberg
-app.get('/download/:id', async (req, res) => {
+app.get("/download/:id", async (req, res) => {
   const bookId = req.params.id;
   const url = `https://www.gutenberg.org/cache/epub/${bookId}/pg${bookId}.txt`;
 
   try {
-    const response = await axios.get(url, { responseType: 'blob' });
-    
-    // Définir les en-têtes de téléchargement
-    res.setHeader('Content-Disposition', `attachment; filename="pg${bookId}.txt"`);
+    const response = await axios.get(url, { responseType: "blob" });
+
+    // Les en-têtes de téléchargement
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="pg${bookId}.txt"`
+    );
     res.send(response.data);
   } catch (error) {
     console.error("Erreur lors du téléchargement du livre:", error);
-    res.status(500).send('Erreur lors du téléchargement du livre');
+    res.status(500).send("Erreur lors du téléchargement du livre");
   }
 });
 
@@ -31,4 +37,3 @@ app.get('/download/:id', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
-
